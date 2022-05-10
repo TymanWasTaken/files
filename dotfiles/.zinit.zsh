2fdba8a7a55c8da3be40b4e44335563dbd8b755a
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-10 07:22:12 -0600

    e

--- dotfiles/.zinit.zsh
+++ dotfiles/.zinit.zsh
@@ -1,10 +1,13 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k; # Powerlevel10k theme
-zinit light anatolykopyl/doas-zsh-plugin; # Double-escape adds doas to command
+#zinit light anatolykopyl/doas-zsh-plugin; # Double-escape adds doas to command
 zinit light Tarrasch/zsh-command-not-found; # Shows command suggestions on invalid
 zinit light Game4Move78/zsh-bitwarden; # Bitwarden cli utilities
 zinit light ianthehenry/zsh-autoquoter; # Auto adds quotes around some commands
 zinit light zdharma-continuum/fast-syntax-highlighting; # Syntax highlighting
-zinit light zsh-users/zsh-autosuggestions # Command suggestions
+zinit light zsh-users/zsh-autosuggestions; # Command suggestions
+zinit light Tarrasch/zsh-bd; # bd command
+zinit light zpm-zsh/colorize; # Colorizes a handfull of comands
+zinit light ellie/atuin; # Better history search
 
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion) # Use history when available, then completion
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh # Load Powerlevel10k config
--- dotfiles/.zprofile
+++ dotfiles/.zprofile
@@ -1,8 +1,12 @@
 #---------------------------------------------------------------------------------#
 ### Fixes
-export PLASMA_USE_QT_SCALING=1 # Make Plasma work properly with high DPI scaling on X11
 export MOZ_USE_XINPUT2=1 # Use pixel-by-pixel touchpad scrolling in Firefox
-export MOZ_ENABLE_WAYLAND=1 # Use experimental native Wayland support for Firefox and Thunderbird
+if [[ $XDG_SESSION_TYPE = 'wayland' ]]; then
+	export MOZ_ENABLE_WAYLAND=1 # Use experimental native Wayland support for Firefox and Thunderbird
+	export QT_QPA_PLATFORM=wayland # Enable QT wayland plugin
+else
+	export PLASMA_USE_QT_SCALING=1 # Make Plasma work properly with high DPI scaling on X11
+fi
 # export GTK_USE_PORTAL=1 # Try to use native file pickers in GTK programs
 #---------------------------------------------------------------------------------#
 ### Load pyenv
